31663e4a42d4302e88a825f9c527a646589d8dc0
Richard Sustek <ennspassion@gmail.com>
2024-02-27 08:38:57 +0100

    release

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,6 +1,6 @@
 
 export const libMetadata = {
 	name: '@kontent-ai-consulting/content-model-accelerator',
-    timestamp: 'Tue, 27 Feb 2024 07:32:45 GMT',
-    version: '0.2.5'
+    timestamp: 'Tue, 27 Feb 2024 07:38:51 GMT',
+    version: '0.2.6'
 };
